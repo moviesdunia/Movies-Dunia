15717a7e6e9838e47110b43107c3dcd8898a694d
moviesdunia <prasanta.eal@gmail.com>
2026-04-26 15:20:28 +0530

    Modify Cloudinary config with new environment variables
    Updated Cloudinary configuration with new credentials.

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,9 @@ const Movie = mongoose.model("Movie", MovieSchema);
 
 /* ================= CLOUDINARY ================= */
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
+  cloud_name: process.env.dsfklevtl,
+  api_key: process.env.149553424498557,
+  api_secret: process.env.nq1hE2YKmCr4h4xDYNWCs1PSwVc
 });
 
 /* ================= MULTER ================= */
